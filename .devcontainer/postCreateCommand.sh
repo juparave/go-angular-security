c01146e64492b5f1a4144d73f7dcab0645d042be
Juparave <juparave@deuxbits.com>
2021-11-20 13:38:30 -0600

    Codespaces. Adding air to workspace in postCreateCommand

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,3 +8,8 @@ cd /workspaces && chown -R $(whoami) . && setfacl -bnR .
 find . -type d -exec echo -n '"{}" ' \; | xargs chmod 755
 find . -type f ! -name "*.sh" -exec echo -n '"{}" ' \; | xargs chmod 644 
 
+# Install air https://github.com/cosmtrek/air
+# binary will be $(go env GOPATH)/bin/air
+curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
+
+air -v
