28b66e9698c075879ccfba8401a70b44ba6b4f66
Juan Pablo Ramírez <juparave@deuxbits.com>
2022-06-09 22:08:10 +0000

    Simplifying app.component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,8 +4,7 @@ import { getCurrentUserAction } from './store/actions/auth.actions';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `<router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
   title = 'angular';
