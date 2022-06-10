3d16a5d0c8667794d582eacdc59100631a8b839b
Juan Pablo Ramírez <juparave@deuxbits.com>
2022-06-10 15:56:33 +0000

    Adding BrowserAnimationsModule

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,9 +20,12 @@ import { authReducer } from './store/reducers/auth.reducers';
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+
     HttpClientModule,
     HttpClientJsonpModule,
     AppRoutingModule,
+
     // StoreModule.forRoot(
     //   {
     //     articles: articleReducers,
