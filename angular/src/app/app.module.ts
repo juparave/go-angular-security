8c7f8d99a485cc6c66df16b02dfb3f9ed691ab2a
Juan Pablo Ramírez <juparave@deuxbits.com>
2022-06-12 17:06:14 +0000

    Adding store.devtools to app module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { articleReducers } from 'src/app/store/reducers/article.reducer';
 import { HeaderComponent } from './header/header.component';
 import { LoginEffect } from './store/effects/auth-login.effects';
@@ -15,6 +16,7 @@ import { LogoutEffect } from './store/effects/auth-logout.effects';
 import { GetCurrentUserEffect } from './store/effects/auth-get-current-user.effects';
 import { RefreshTokensEffect } from './store/effects/auth-refresh-tokens.effects';
 import { authReducer } from './store/reducers/auth.reducers';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -42,6 +44,10 @@ import { authReducer } from './store/reducers/auth.reducers';
       GetCurrentUserEffect,
       RefreshTokensEffect,
     ]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
