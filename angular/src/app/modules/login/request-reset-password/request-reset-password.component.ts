f34f667ec02f9c41af6aa7fea5705cc3d4c5972a
Juan Pablo Ramírez <juparave@deuxbits.com>
2022-07-14 22:50:39 +0000

    Transport... request reset password form

--- angular/src/app/modules/login/request-reset-password/request-reset-password.component.ts
+++ angular/src/app/modules/login/request-reset-password/request-reset-password.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/store/interfaces/app-state';
+import { BackendErrors } from 'src/app/store/types/backend-errors.interface';
 
 @Component({
   selector: 'app-request-reset-password',
@@ -6,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-reset-password.component.scss']
 })
 export class RequestResetPasswordComponent implements OnInit {
+  form: FormGroup;
+  isSubmitting$!: Observable<boolean>;
+  backendErrors$!: Observable<BackendErrors | null>;
 
-  constructor() { }
+  constructor(private store: Store<AppState>, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  doRequestResetPassword() {
+  }
 }
--- angular/src/app/modules/login/reset-password/reset-password.component.ts
+++ angular/src/app/modules/login/reset-password/reset-password.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { requestResetPasswordAction } from 'src/app/store/actions/auth.actions';
+import { AppState } from 'src/app/store/interfaces/app-state';
+import { isSubmittingSelector, validationErrorsSelector } from 'src/app/store/selectors/auth.selectors';
+import { BackendErrors } from 'src/app/store/types/backend-errors.interface';
+import { RequestResetPassword } from 'src/app/store/types/request-reset.interface';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
-  styleUrls: ['./reset-password.component.scss']
+  styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent implements OnInit {
+  form: FormGroup;
+  isSubmitting$!: Observable<boolean>;
+  backendErrors$!: Observable<BackendErrors | null>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<AppState>, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {
+    this.initializeValues();
+  }
+
+  private initializeValues() {
+    this.isSubmitting$ = this.store.select(isSubmittingSelector);
+    this.backendErrors$ = this.store.select(validationErrorsSelector);
+  }
+
+  doRequestResetPassword() {
+    if (this.form.valid) {
+      const request: RequestResetPassword = this.form.value;
+      this.store.dispatch(requestResetPasswordAction({ request }));
+    }
+  }
 }
