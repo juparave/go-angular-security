401c7e078f05bc082fe1ac3e237f923c2f09668e
Juparave <juparave@deuxbits.com>
2022-10-17 17:46:14 -0500

    Updating dev container to Ubutu Jammy

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -12,8 +12,6 @@ cd /workspaces && chown -R $(whoami) . && sudo setfacl -bnR . \
 # binary will be $(go env GOPATH)/bin/air
 curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
 
-air -v
-
 # Install gh CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
